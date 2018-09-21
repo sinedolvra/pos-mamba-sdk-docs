d63881969bfdcabf049b491bf8788160bf89cc6e
Raphael Cardoso <5193273+raphaelcardoso@users.noreply.github.com>
2018-09-21 09:50:11 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,25 +7,9 @@ import compression from 'compression';
 import { Store } from 'svelte/store.js';
 import { manifest } from './manifest/server.js';
 import http from 'http';
-import https from 'https';
 import fs from 'fs';
 
 dotenv.config();
-
-const privateKey = fs.readFileSync('/home/deployusr/cert/_.stone.com.br.key');
-
-const certificate = fs.readFileSync('/home/deployusr/cert/_.stone.com.br.crt');
-
-const ca = [
-	     fs.readFileSync('/home/deployusr/cert/gd_bundle-g2-g1.crt'),
-	     fs.readFileSync('/home/deployusr/cert/be94a68b8af575bd.crt')
-	   ]
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-}
 
 const app = express();
 
@@ -44,10 +28,4 @@ app
   )
 
 const httpServer = http.createServer(app);
-
-const httpsServer = https.createServer(credentials, app);
-
-
-httpServer.listen(80);
-
-httpsServer.listen(443);
+httpServer.listen(process.env.PORT);
